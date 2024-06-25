7f1cabc28adb795f2e77d9c93611772719baa97f
EmanVB <emanbonsato@yahoo.com>
2024-06-25 18:10:44 +0800

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,9 +1,14 @@
-export const Section = ({ title, children}) => {
-    return (
-        <>
-            <h2>{title}</h2>
-            <section>{children}</section>
-        </> 
-  )
-}
- 
+import PropTypes from 'prop-types';
+export const Section = ({ title, children }) => {
+  return (
+    <>
+      <h2>{title}</h2>
+      <section>{children}</section>
+    </>
+  );
+};
+
+Section.propTypes = {
+  title: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
