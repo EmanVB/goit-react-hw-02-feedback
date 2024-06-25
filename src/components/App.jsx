9a0309d9207bd530c8f025cddf2e828398a008f6
EmanVB <emanbonsato@yahoo.com>
2024-06-25 17:34:32 +0800

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,9 @@
-import { Component } from 'react'
+import { Component } from 'react';
 import { Statistics } from './Statistics/Statistics';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Section } from './Section/Section';
 
 export class App extends Component {
-  
   state = {
     good: 0,
     neutral: 0,
@@ -23,31 +22,36 @@ export class App extends Component {
     return good + neutral + bad;
   };
 
-
   countPositiveFeedbackPercentage = () => {
     const { good, neutral, bad } = this.state;
-    return good / (good + neutral + bad)
+    return good / (good + neutral + bad);
   };
-  
 
   render() {
     const { good, neutral, bad } = this.state;
     const total = this.countTotalFeedback();
-    const options = ["good", "neutral", "bad"];
-    const positive = (this.countPositiveFeedbackPercentage()) * 100;
-  
+    const options = ['good', 'neutral', 'bad'];
+    const positive = (this.countPositiveFeedbackPercentage() * 100).toFixed(1);
+
     return (
       <>
         <Section title="Please Leave Feedback">
-          <FeedbackOptions options={options} onLeaveFeedback={this.handleClick} />
+          <FeedbackOptions
+            options={options}
+            onLeaveFeedback={this.handleClick}
+          />
         </Section>
-        
+
         <Section title="Statistics">
-          <Statistics good={good} neutral={neutral} bad={bad} total={total} positive={positive} />
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={total}
+            positive={positive}
+          />
         </Section>
-        
       </>
-    )
+    );
   }
 }
-
