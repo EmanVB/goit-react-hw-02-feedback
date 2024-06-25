75b0e2c4e5753172bbd598aae258bd23a8ec9757
EmanVB <emanbonsato@yahoo.com>
2024-06-25 18:40:08 +0800

    Create css module for components

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,21 +1,30 @@
+import PropTypes from 'prop-types';
 export const Statistics = ({ good, neutral, bad, total, positive }) => {
   return (
-      <div>
-          <p>
-              Good: <span>{good}</span>
-          </p>
-          <p>
-              Neutral: <span>{neutral}</span>
-          </p>
-          <p>
-              Bad: <span>{bad}</span>
-          </p>
-          <p>
-              Total: <span>{total}</span>
-          </p>
-          <p>
-              Positive Feedback: <span>{positive}%</span>
-          </p>
+    <div>
+      <p>
+        Good: <span>{good}</span>
+      </p>
+      <p>
+        Neutral: <span>{neutral}</span>
+      </p>
+      <p>
+        Bad: <span>{bad}</span>
+      </p>
+      <p>
+        Total: <span>{total}</span>
+      </p>
+      <p>
+        Positive Feedback: <span>{positive}%</span>
+      </p>
     </div>
-  )
-}
+  );
+};
+
+Statistics.propTypes = {
+  good: PropTypes.number.isRequired,
+  neutral: PropTypes.number.isRequired,
+  bad: PropTypes.number.isRequired,
+  total: PropTypes.number.isRequired,
+  positive: PropTypes.string.isRequired,
+};
